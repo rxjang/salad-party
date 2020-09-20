11cb067d52cb42446404741a8c2f357e36c2bccb
rxjang <rxforp@naver.com>
2020-09-20 15:33:18 +0900

    temp

---
+++ bookery/src/main/java/co/salpa/bookery/model/MylibDao.java
@@ -0,0 +1,15 @@
+package co.salpa.bookery.model;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import co.salpa.bookery.model.entity.BookVo;
+
+public interface MylibDao {
+	
+	List<BookVo> selectNoGoalBook(int id) throws DataAccessException;
+	List<BookVo> selectStudyingBook(int id) throws DataAccessException;
+	List<BookVo> selectFinishedBook(int id) throws DataAccessException;
+	
+}
