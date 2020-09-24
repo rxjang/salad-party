802bbeb2277398cb4247f8ef6e68d47e00341717
jhj7176 <jhj7176@gmail.com>
2020-09-24 20:34:45 +0900

    page, pageResult

---
+++ bookery/src/main/java/co/salpa/bookery/model/entity/CheckPageVo.java
@@ -0,0 +1,33 @@
+package co.salpa.bookery.model.entity;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@NoArgsConstructor
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class CheckPageVo {
+
+	private int id;
+	private int planpage;
+	
+	@NonNull
+	private int actualpage;
+	@NonNull
+	private int study_id;
+	@NonNull
+	private Date date;
+	@NonNull
+	private int delete;
+
+}
--- bookery/src/main/java/co/salpa/bookery/today/service/TodayPageService.java
+++ bookery/src/main/java/co/salpa/bookery/today/service/TodayPageService.java
@@ -8,8 +8,11 @@ import org.springframework.ui.Model;
 import co.salpa.bookery.model.entity.V_StudyVo;
 
 public interface TodayPageService {
-	
-	Model listV_StudyService(Model model) throws SQLException;
-	Model getV_StudyService(int user_id,int bid, Model model) throws SQLException;
 
+	Model listV_StudyService(Model model) throws SQLException;
+
+	Model getV_StudyService(int user_id, int bid, Model modsel) throws SQLException;
+
+	void checkPageService(int actualpage, int study_id) throws SQLException;
+	
 }
