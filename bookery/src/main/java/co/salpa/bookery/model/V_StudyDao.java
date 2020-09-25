48ad19d7c6b4de225264441c63bd1777496cbb1a
rxjang <rxforp@naver.com>
2020-09-25 15:29:43 +0900

    mylib edit

--- bookery/src/main/java/co/salpa/bookery/model/V_StudyDao.java
+++ bookery/src/main/java/co/salpa/bookery/model/V_StudyDao.java
@@ -4,6 +4,9 @@ import java.sql.SQLException;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.dao.DataAccessException;
+
+import co.salpa.bookery.model.entity.BookVo;
 import co.salpa.bookery.model.entity.V_StudyVo;
 
 public interface V_StudyDao {
@@ -13,4 +16,10 @@ public interface V_StudyDao {
 	V_StudyVo selectOneByStudyId(int study_id) throws SQLException;
 	List<V_StudyVo> selectAllByUserId(int key) throws SQLException;
 	List<V_StudyVo> selectActiveByUserId(int key) throws SQLException;
+	List<V_StudyVo> selectNoGoalBook(int id) throws DataAccessException; //미독 책 리스트 반환
+	List<V_StudyVo> selectStudyingBook(int id) throws DataAccessException; //미완독 책 리스트 반환
+	List<V_StudyVo> selectFinishedBook(int id) throws DataAccessException; //완독 책 리스트 반환
+	int countNoGoalBook(int id) throws DataAccessException; //미독 책 개수 반환
+	int countStudyingBook(int id) throws DataAccessException; //미완독 책 개수 반환
+	int countFinishedBook(int id) throws DataAccessException; //완독 책 개수 반환
 }
--- bookery/src/main/java/co/salpa/bookery/mylib/service/MylibService.java
+++ bookery/src/main/java/co/salpa/bookery/mylib/service/MylibService.java
@@ -5,10 +5,5 @@ import org.springframework.dao.DataAccessException;
 import org.springframework.ui.Model;
 
 public interface MylibService {
-	Model listNoGoalBookService(Model model) throws DataAccessException;
-	Model listStudyingBookService(Model model) throws DataAccessException;
-	Model listFinishedBookService(Model model) throws DataAccessException;
-	Model countNoGoalBookService(Model model) throws DataAccessException;
-	Model countStudyingBookService(Model model) throws DataAccessException;
-	Model countFinishedBookService(Model model) throws DataAccessException;
+	Model myLibService(Model model) throws DataAccessException;
 }
