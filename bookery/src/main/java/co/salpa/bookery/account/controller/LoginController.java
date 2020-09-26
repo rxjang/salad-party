dc864df4cb8ed7882d5c40522f705b693534cd09
jiwoo <kamja227@naver.com>
2020-09-26 15:10:50 +0900

    login finished

--- bookery/src/main/java/co/salpa/bookery/account/controller/LoginController.java
+++ bookery/src/main/java/co/salpa/bookery/account/controller/LoginController.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import co.salpa.bookery.account.AccountService;
 import co.salpa.bookery.model.entity.UserVo;
@@ -32,27 +32,33 @@ public class LoginController {
 		else 
 			return "redirect:../";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(HttpSession session, HttpServletResponse resp, @RequestParam("email") String email, @RequestParam("password") String password) throws SQLException {
+//	public String login(HttpSession session, HttpServletResponse resp, @RequestParam("email") String email, @RequestParam("password") String password) throws SQLException {
+	public String login(HttpSession session, HttpServletResponse resp, @ModelAttribute UserVo bean) 
+			throws SQLException {
 
 		UserVo userBean = null;
-		userBean = accountService.login(email, password);
+		userBean = accountService.login(bean.getEmail(), bean.getPassword());
 		System.out.println(userBean);
 		
 		if(userBean != null) {
 			session.setAttribute("user", userBean);
-			return "redirect:../";
+			try {
+				resp.getWriter().write("{\"result\":\"success\"}");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 		else {
 			try{
-				resp.setContentType("application / json; charset = utf-8");
+//				resp.setContentType("application / json; charset = utf-8");
 				resp.getWriter().write("{\"result\":\"fail\"}");
 			} catch (IOException e) {
                 e.printStackTrace();
             }
-			return "redirect:./login";
 		}
+		return null;
 	}
 	
 	@RequestMapping("/navercallback")
