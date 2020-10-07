a312c1218bedff494101cf0532b2fc3bfddeda15
Kate <hyeongjoo.kim@gmail.com>
2020-10-07 15:00:10 +0900

    Calendar updated

---
+++ bookery/src/main/java/co/salpa/bookery/model/V_CalendarDao.java
@@ -0,0 +1,13 @@
+package co.salpa.bookery.model;
+
+import java.util.List;
+
+import org.springframework.dao.DataAccessException;
+
+import co.salpa.bookery.model.entity.V_CalendarVo;
+
+public interface V_CalendarDao {
+	List<V_CalendarVo> selectAllByUserId(int user_id) throws DataAccessException;
+
+	List<V_CalendarVo> selectAllByStudyId(int study_id) throws DataAccessException;
+}
